21d20d4bbab7481d30db5c2cbe9e4268b11cbc36
Abhishek Grover <Abhishek.Grover@smartenergywater.in>
2020-10-05 01:10:41 +0530

    Initial Commit

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Values> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Values>().HasData(
+                new Values { Id = 1, Name = "Abhishek" }
+            );
+        }
+    }
+}
